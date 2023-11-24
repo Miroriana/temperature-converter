4a4c092c8aceb9c779037086790026d366225744
miroriana <shumbushoihiclaudia@gmail.com>
2023-11-24 09:51:41 +0100

    creation of the file

---
+++ temperature_converter.js
@@ -0,0 +1,11 @@
+// Write a function that converts a temperature from Celsius to Fahrenheit
+//     ### Formula:
+//         tFahrenheit = (tCelcius°C × 9/5) + 32
+        
+//         Where 
+//             tFahrenheit: Temperature in Fahrenheit
+//             tCelcius: Temperature in Celcius 
+
+function temperatureConverter(){
+    
+}
